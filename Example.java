e07678b52065bd7c6479a1ea48cc6fdbb06d2aa3
abhishekmatcha <seshaabhishek@gmail.com>
2017-04-10 19:00:20 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,10 @@
+class Example{
+		 int x; int y;
+		 String name;
+		 public static void main(String args[]) {
+		 Example pnt = new Example();
+		 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+		} }
+// correct option d
+//d. The program prints pnt is null 0 
+//Explanation:Instance variable of type int and String are initialized to 0 
